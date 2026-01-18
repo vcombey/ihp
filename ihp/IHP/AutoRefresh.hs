607cd765eb566225ee03b36c88c206e5294b572a
vcombey <vincent.combey@gmail.com>
2026-01-18 11:29:46 +0100

    tests

--- ihp/IHP/AutoRefresh.hs
+++ ihp/IHP/AutoRefresh.hs
@@ -22,6 +22,7 @@ import qualified Control.Exception as Exception
 import qualified Control.Concurrent.MVar as MVar
 import qualified Data.Maybe as Maybe
 import qualified Data.Text as Text
+import qualified Data.Text.Lazy as LazyText
 import qualified Data.Text.Encoding as TextEncoding
 import qualified Data.Text.Encoding.Error as TextEncodingError
 import IHP.WebSocket
@@ -34,7 +35,7 @@ import qualified IHP.Log as Log
 import qualified Data.Vault.Lazy as Vault
 import System.IO.Unsafe (unsafePerformIO)
 import Network.Wai
-import Network.HTTP.Types.Header (hContentType)
+import Network.HTTP.Types.Header (ResponseHeaders, hContentType)
 import qualified Text.Blaze.Html.Renderer.Text as BlazeText
 
 initAutoRefresh :: (?context :: ControllerContext) => IO ()
@@ -276,18 +277,18 @@ notificationTrigger tableName = PG.Query [i|
 ensureAutoRefreshMeta :: (?context :: ControllerContext) => ResponseHeaders -> LByteString -> IO LByteString
 ensureAutoRefreshMeta headers html
     | not (isHtmlResponse headers) = pure html
-    | LBS.isInfixOf "ihp-auto-refresh-id" html = pure html
+    | BS.isInfixOf "ihp-auto-refresh-id" (LBS.toStrict html) = pure html
     | otherwise = do
         meta <- renderAutoRefreshMeta
         if LBS.null meta
             then pure html
-            else pure (insertAutoRefreshMeta meta html)
+            else pure (insertAutoRefreshMetaForRequest meta html)
 
 renderAutoRefreshMeta :: (?context :: ControllerContext) => IO LByteString
 renderAutoRefreshMeta = do
     frozenContext <- freeze ?context
     let ?context = frozenContext
-    let metaText = BlazeText.renderHtml autoRefreshMeta
+    let metaText = LazyText.toStrict (BlazeText.renderHtml autoRefreshMeta)
     pure (encodeUtf8Text metaText)
 
 insertAutoRefreshMeta :: LByteString -> LByteString -> LByteString
@@ -296,6 +297,18 @@ insertAutoRefreshMeta meta html =
         htmlText = decodeUtf8Text html
         resultText = insertMetaText metaText htmlText
     in encodeUtf8Text resultText
+
+insertAutoRefreshMetaForRequest :: (?context :: ControllerContext) => LByteString -> LByteString -> LByteString
+insertAutoRefreshMetaForRequest meta html =
+    if isHtmxRequest
+        then insertAutoRefreshMetaFragment meta html
+        else insertAutoRefreshMeta meta html
+
+insertAutoRefreshMetaFragment :: LByteString -> LByteString -> LByteString
+insertAutoRefreshMetaFragment meta html =
+    let metaText = decodeUtf8Text meta
+        htmlText = decodeUtf8Text html
+    in encodeUtf8Text (metaText <> htmlText)
 
 insertMetaText :: Text -> Text -> Text
 insertMetaText meta html =
@@ -321,6 +334,9 @@ isHtmlResponse :: ResponseHeaders -> Bool
 isHtmlResponse headers = case lookup hContentType headers of
     Just value -> "text/html" `BS.isPrefixOf` value
     Nothing -> False
+
+isHtmxRequest :: (?context :: ControllerContext) => Bool
+isHtmxRequest = isJust (getHeader "HX-Request")
 
 decodeUtf8Text :: LByteString -> Text
 decodeUtf8Text = TextEncoding.decodeUtf8With TextEncodingError.lenientDecode . LBS.toStrict
